2a5fef65ac820318a039db776c897df7b81dbd1c
SrGeek <jaimemvaldez14@gmail.com>
2020-08-30 19:47:42 -0500

    Update Configuration_adv.h
    Corrected bed and probe limits

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -598,9 +598,9 @@
 #define DUAL_X_CARRIAGE
 #if ENABLED(DUAL_X_CARRIAGE)
   #define X1_MIN_POS X_MIN_POS   // Set to X_MIN_POS
-  #define X1_MAX_POS X_BED_SIZE-36  // Set a maximum so the first X-carriage can't hit the parked second X-carriage
+  #define X1_MAX_POS X_BED_SIZE-39  // Set a maximum so the first X-carriage can't hit the parked second X-carriage
   #define X2_MIN_POS    40       // Set a minimum to ensure the  second X-carriage can't hit the parked first X-carriage
-  #define X2_MAX_POS   455+23    // Set this to the distance between toolheads when both heads are homed
+  #define X2_MAX_POS   X_BED_SIZE+21    // Set this to the distance between toolheads when both heads are homed
   #define X2_HOME_DIR    1       // Set to 1. The second X-carriage always homes to the maximum endstop position
   #define X2_HOME_POS X2_MAX_POS // Default X2 home position. Set to X2_MAX_POS.
                       // However: In this mode the HOTEND_OFFSET_X value for the second extruder provides a software
@@ -1649,10 +1649,10 @@
  * the probe to be unable to reach any points.
  */
 #if PROBE_SELECTED && !IS_KINEMATIC
-  #define PROBING_MARGIN_LEFT PROBING_MARGIN
-  #define PROBING_MARGIN_RIGHT 4*PROBING_MARGIN
-  #define PROBING_MARGIN_FRONT PROBING_MARGIN
-  #define PROBING_MARGIN_BACK PROBING_MARGIN
+  #define PROBING_MARGIN_LEFT 2.5*PROBING_MARGIN
+  #define PROBING_MARGIN_RIGHT 6*PROBING_MARGIN
+  #define PROBING_MARGIN_FRONT PROBING_MARGIN/2
+  #define PROBING_MARGIN_BACK 13*PROBING_MARGIN
 #endif
 
 #if EITHER(MESH_BED_LEVELING, AUTO_BED_LEVELING_UBL)
@@ -3113,7 +3113,7 @@
 //#define NO_WORKSPACE_OFFSETS
 
 // Extra options for the M114 "Current Position" report
-//#define M114_DETAIL         // Use 'M114` for details to check planner calculations
+#define M114_DETAIL         // Use 'M114` for details to check planner calculations
 //#define M114_REALTIME       // Real current position based on forward kinematics
 //#define M114_LEGACY         // M114 used to synchronize on every call. Enable if needed.
 
